dcbd528414ea39baf72333bb58c94f7cf7c6eb49
Julien Richard <jairbubbles@hotmail.com>
2017-04-30 13:57:10 +0200

    Initial commit

---
+++ Comparer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var frameworks = new[] { "4.6", "4.6.1", "4.6.2" };
+            foreach (var framework in frameworks)
+            {
+                var exePath = $@"../../../RoslynCompiler_{framework}\bin\Release\RoslynCompiler.exe";
+
+                Console.WriteLine($"== Run Roslyn Compiler with .NET Framework {framework} ==");
+
+                Console.WriteLine(" Creating startup profile:");
+                LaunchCompiler(exePath, true);
+                LaunchCompiler(exePath, true);
+                LaunchCompiler(exePath, true);
+
+                Console.WriteLine(" Use startup profile:");
+                LaunchCompiler(exePath);
+                LaunchCompiler(exePath);
+                LaunchCompiler(exePath);
+
+                Console.WriteLine("");
+            }
+        }
+
+        private static void LaunchCompiler(string exePath, bool deleteStartupProfile = false)
+        {
+            if (deleteStartupProfile)
+            {
+                var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var profileFolder = Path.Combine(appData, "RoslynCompiler");
+                if (Directory.Exists(profileFolder))
+                {
+                    Directory.Delete(profileFolder, true);
+                }
+            }
+
+            var w = new Stopwatch();
+            w.Start();
+
+            Console.Write("  ");
+
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = exePath,
+                UseShellExecute = false,
+            }).WaitForExit();
+
+            Console.WriteLine($"  Execution time: {w.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Semantics;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime;
+
+namespace RoslynCompiler
+{
+    public class Program
+    {
+        public static Assembly Compile(params string[] sources)
+        {
+            var assemblyFileName = "gen" + Guid.NewGuid().ToString().Replace("-", "") + ".dll";
+            var compilation = CSharpCompilation.Create(assemblyFileName,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
+                syntaxTrees: from source in sources
+                             select CSharpSyntaxTree.ParseText(source),
+                references: from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                            where !string.IsNullOrEmpty(assembly.Location)
+                            select MetadataReference.CreateFromFile(assembly.Location)
+                            );
+
+
+            EmitResult emitResult;
+
+            using (var dll = new MemoryStream())
+            {
+                emitResult = compilation.Emit(dll, options: new EmitOptions(false, DebugInformationFormat.Embedded));
+
+                if (emitResult.Success)
+                {
+                    var assembly = Assembly.Load(dll.GetBuffer());
+                    return assembly;
+                }
+            }
+
+            var message = string.Join("\r\n", emitResult.Diagnostics);
+            throw new ApplicationException(message);
+        }
+
+        public static string simpleExample =
+            @"using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(""test"");
+    }
+}
+ ";
+
+        public static Assembly SimpleCompilation()
+        {
+            return Compile(new string[] { simpleExample });
+        }
+
+        static void Main(string[] args)
+        {
+            // Enable Multicore JIT (https://blogs.msdn.microsoft.com/dotnet/2012/10/18/an-easy-solution-for-improving-app-launch-performance/)
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var profileFolder = Path.Combine(appData, "RoslynCompiler");
+            Directory.CreateDirectory(profileFolder);
+            ProfileOptimization.SetProfileRoot(profileFolder);
+            ProfileOptimization.StartProfile("Startup.Profile");
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            SimpleCompilation();
+            Console.Write($"Compilation time: {stopWatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
